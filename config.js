97764ee24bd441363477fc3a32f443357efc1bf9
remyL <remy@envergure-digitale.fr>
2019-02-12 12:46:31 +0100

    add 2sec betwin request -> 8h for 13000 call

--- config.js
+++ config.js
@@ -1,8 +1,9 @@
-module.exports = {
-	steamLogin: '', 
-	steamPassWord: '', 
-	dbHost: '',
-	dbUsername: '', 
-	dbPassWord:'', 
-	dbName:'',
+let config = {
+	steamLogin: 'thorfy3', 
+	steamPassWord: 'Xki0133e', 
+	dbHost: '172.17.0.2',
+	dbUsername: 'root', 
+	dbPassWord:'admin', 
+	dbName:'steamcrawler',
 };
+module.exports = config;
--- index.js
+++ index.js
@@ -3,31 +3,32 @@ const request = require('request');
 const moment = require('moment');
 
 const Item = require('./classes/item.js');
-const Database = require('./classes/db.js');
 const config = require('./config.js');
+const Database = require('./classes/db.js');
 
 
 let db = new Database();
 
  //REFRESH ALL DB ITEM
-/* Item.getAllItems().then(res => { 
- 	if(res.success === true){
- 		res.items.forEach(function(obj) {
- 			try {
- 				sqlrequest = `INSERT INTO item (market_name, market_hash_name, border_color, image) VALUES ("${obj.market_name}", "${obj.market_hash_name}", "${obj.name_color}", "https:${obj.icon_url}");`;
- 				db.query(sqlrequest).then( result => console.log(result));
- 			}catch(error){
- 				console.error(error);
- 			}
- 		});
+ // Item.getAllItems().then(res => { 
+ // 	if(res.success === true){
+ // 		res.items.forEach(function(obj) {
+ // 			try {
+ // 				sqlrequest = `INSERT INTO item (market_name, market_hash_name, border_color, image) VALUES ("${obj.market_name}", "${obj.market_hash_name}", "${obj.name_color}", "https:${obj.icon_url}");`;
+ // 				db.query(sqlrequest).then( result => console.log(result));
+ // 			}catch(error){
+ // 				console.error(error);
+ // 			}
+ // 		});
 
- 	}else{
- 		console.log(res.message);
- 	}
- });
- */
+ // 	}else{
+ // 		console.log(res.message);
+ // 	}
+ // });
+ 
 
  let client = new SteamUser();
+
  client.logOn({	
  	"accountName": config.steamLogin,
  	"password": config.steamPassWord
@@ -45,11 +46,11 @@ let db = new Database();
 
 	db.query('SELECT * FROM `item`').then(function(items){
 		asyncForEach(items, async (item) => {
-			let pause = await setTimeout(function(){ console.log(item.market_hash_name + ' - request'); }, 1500);
+
+			await sleep(2000);
+			console.log(item.market_hash_name +' - requested'y)
 			let res = await Item.getItemHistory(item.market_hash_name, cookies);
 			
-
-			}
 			if(res.success === true){
 				let bigInsert = "";
 				res.prices.forEach(async function(arrayData) {
@@ -64,41 +65,6 @@ let db = new Database();
 
 		})
 	});
-
-
-
-
-/*	db.query('SELECT * FROM `item`').then(function(items){
-		let itemsArray = JSON.parse(JSON.stringify(items));
-		itemsArray.forEach(function(item){
-			if(item.market_hash_name){
-				try{
-					Item.getItemHistory(item.market_hash_name, cookies).then(res => {
-						if(res.success === true){
-							let sqlrequest = "";
-							res.prices.forEach(function(arrayData) {
-								sqlrequest += `INSERT INTO history (id_item, date, price, volume) VALUES (${item.id_item}, "${arrayData[0]}", "${arrayData[1]}", "${arrayData[2]}");`;
-							});
-							try{
-								db.query(sqlrequest).then(function(response){
-									console.log(`${item.market_hash_name} added in db`);
-								})
-							}catch(e){
-								console.log(e)
-							}
-						}else{
-							console.log(res.message);
-						}
-
-					})
-
-				}catch(e){
-					console.log(e);
-				}
-			}
-		})
-	});
-	*/
 });
 
 
@@ -107,3 +73,6 @@ let db = new Database();
  		await callback(array[index], index, array);
  	}
  }
+ function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
